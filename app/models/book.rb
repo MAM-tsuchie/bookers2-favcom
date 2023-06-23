b6df29130c348e3d165a4377b6cdc0a0e24efd0d
TsuchieShinya <89980497+MAM-tsuchie@users.noreply.github.com>
2023-06-23 08:30:57 +0000

    ①Bookers2にいいね機能を追加しましょう

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user # commit 79b3044で修正済み
+  has_many :favorites, dependent: :destroy
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
