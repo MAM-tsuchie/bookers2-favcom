e4c233fa14b4a03ca2c67f896a281ae57203db86
TsuchieShinya <89980497+MAM-tsuchie@users.noreply.github.com>
2023-06-23 09:56:59 +0000

    ②Bookers2にコメント機能を追加しましょう

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user # commit 79b3044で修正済み
+  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   validates :title,presence:true
